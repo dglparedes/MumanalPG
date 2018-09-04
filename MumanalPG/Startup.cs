899068a7233f333908821cee0bf19a2619a29923
Ronald Flores <r.flores.m@hotmail.com>
2018-09-03 22:09:28 -0400

    MIGRATION FOR POSTGRES
    Fix Migrations Identity for Postgres

--- MumanalPG/Startup.cs
+++ MumanalPG/Startup.cs
@@ -27,15 +27,16 @@ namespace MumanalPG
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<CookiePolicyOptions>(options =>
+			var connectionString = Configuration.GetConnectionString("DefaultConnection");
+			
+			services.Configure<CookiePolicyOptions>(options =>	
 			{
 				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
 				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
-			//services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-			services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 			services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
